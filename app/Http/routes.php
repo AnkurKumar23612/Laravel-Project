26113d7f8ed9bda21a6f0e1b50f1b17988e51a74
Ankur <ankur.kumar23612@gmail.com>
2020-07-30 01:12:58 +0530

    admin setup, gulp setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::auth();
+
+Route::get('/home', 'HomeController@index');
+
+
+Route::resource('admin/users', 'AdminUsersController');
+
+
+Route::get('/admin', function () {
+
+
+    return view('admin.index');
+});
