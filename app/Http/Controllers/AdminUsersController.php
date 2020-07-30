7febf83266ade50481c86fee597a4ce2d3f65f3d
Ankur <ankur.kumar23612@gmail.com>
2020-07-31 01:48:58 +0530

    all users part completed inluding security

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Requests\UsersRequest;
 use App\Http\Requests\UsersEditRequest;
-use App\User;
-use App\Role;
+use App\Http\Requests\UsersRequest;
 use App\Photo;
+use App\Role;
+use App\User;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 
 class AdminUsersController extends Controller
@@ -183,5 +187,19 @@ class AdminUsersController extends Controller
     public function destroy($id)
     {
         //
+
+          $user = User::findOrFail($id);
+
+
+        unlink(public_path() . $user->photo->file);
+
+
+        $user->delete();
+
+
+        Session::flash('deleted_user','The user has been deleted');
+
+
+        return redirect('/admin/users');
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,11 +20,26 @@ Route::auth();
 Route::get('/home', 'HomeController@index');
 
 
-Route::resource('admin/users', 'AdminUsersController');
 
 
-Route::get('/admin', function () {
+// Route::get('/admin', function () {
 
 
-    return view('admin.index');
-});
+//     return view('admin.index');
+//  });
+
+
+Route::group(['middleware'=>'admin'], function(){
+
+
+	Route::get('/admin', function(){
+
+        return view('admin.index');
+
+
+    });
+
+
+     Route::resource('admin/users', 'AdminUsersController');
+
+     });
