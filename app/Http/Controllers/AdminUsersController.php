3548cf7bf172375bfadf1cc9dca885f3fd335dd9
Ankur <ankur.kumar23612@gmail.com>
2020-07-31 00:17:07 +0530

    almost all users process completed

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Requests\UsersRequest;
+use App\Http\Requests\UsersEditRequest;
 use App\User;
 use App\Role;
 use App\Photo;
@@ -109,7 +110,13 @@ class AdminUsersController extends Controller
     {
         //
 
-        return view('admin.users.edit');
+       $user = User::findOrFail($id);
+
+        $roles = Role::lists('name','id')->all();
+
+
+        return view('admin.users.edit', compact('user','roles'));
+
     }
 
     /**
@@ -119,9 +126,52 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
         //
+
+         $user = User::findOrFail($id);
+
+
+        if(trim($request->password) == ''){
+
+            $input = $request->except('password');
+
+        } else{
+
+
+            $input = $request->all();
+
+            $input['password'] = bcrypt($request->password);
+
+        }
+
+
+
+
+        if($file = $request->file('photo_id')){
+
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file'=>$name]);
+
+
+            $input['photo_id'] = $photo->id;
+
+
+        }  
+
+
+
+        $user->update($input);
+
+
+        return redirect('/admin/users');
+
+
     }
 
     /**
